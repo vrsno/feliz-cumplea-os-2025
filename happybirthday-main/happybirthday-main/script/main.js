7f5e7bbc39cb78ab4a9128d5035cfac99dc409c9
sinname <nexxuz2017@gmail.com>
2025-07-11 17:29:18 -0500

    falta music and link

--- happybirthday-main/happybirthday-main/script/main.js
+++ happybirthday-main/happybirthday-main/script/main.js
@@ -1,12 +1,12 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Quieres reproducir la musica de fondo?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
+        confirmButtonText: 'Si',
         cancelButtonText: 'No',
     }).then((result) => {
         if (result.isConfirmed) {
